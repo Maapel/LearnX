d4d7b3c924c73df314a6f91c2fd0b169a26469a9
Maapel <maapel1703@gmail.com>
2025-08-30 01:07:46 +0530

    Initial commit: Project setup

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('LearnX Backend API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
